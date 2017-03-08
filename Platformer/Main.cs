7afcb58b4cb3d45c6ff6807a22ba0a7f44ca56f9
Gravechapa <Gravechapa@protonmail.com>
2017-03-08 10:55:43 +0300

    First

---
+++ Platformer/Main.cs
@@ -0,0 +1,17 @@
+﻿using SFML.Window;
+using SFML.Graphics;
+class MainClass
+{
+	static int Main()
+	{
+		var window = new RenderWindow(new VideoMode(200, 200), "test");
+		window.Closed += (sender, e) => window.Close();
+		while (window.IsOpen)
+		{
+			window.DispatchEvents();
+			window.Clear();
+			window.Display();
+		}
+		return 0;
+	}
+};
